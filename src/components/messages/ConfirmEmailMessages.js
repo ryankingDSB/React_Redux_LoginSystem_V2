52a44a5f60ec9160d9d15e063b0b193df8416bfc
Ryan King <ryan.king@deepskyblue.com>
2018-01-26 08:25:19 +0000

    Redirect routes complete

---
+++ src/components/messages/ConfirmEmailMessages.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Message } from 'semantic-ui-react';
+
+const ConfirmEmailMessage = () => (
+    <Message info>
+        <Message.Header>Please, verify your email to unlock this page</Message.Header>
+    </Message>
+);
+
+export default ConfirmEmailMessage;
+
